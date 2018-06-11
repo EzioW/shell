164edd3eb20ce4a3da52663225a7b58f23f26e65
于新哲 <yuxinzhe@meituan.com>
2018-06-11 16:37:21 +0800

    add one key tag

---
+++ oneKeyTag.sh
@@ -0,0 +1,29 @@
+function oneKeyTag() {
+  echo -e "\e[1;33m choose tag type \e[0m";
+  select tagType in hotfix;
+  do
+    break
+  done
+  
+  now=$(date +"%Y%m%d%H%M");
+
+  branchName=`git symbolic-ref --short -q HEAD`;
+
+  tagName="$tagType-$now-$branchName";
+
+  echo -e "\e[1;32m create tag name Done \e[0m";
+  echo "$tagName";
+
+  echo -e "\e[1;33m input tag info \e[0m";
+  read msg;
+
+  git tag -a $tagName -m msg;
+  gp origin $tagName;
+  tagStatusCode=$?;
+
+  if [ $tagStatusCode -eq 0 ]; then
+  echo -e "\e[1;32m git create tag Done \e[0m";
+  fi
+}
+
+alias okt='oneKeyTag'
